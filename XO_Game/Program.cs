55be52eddd61c27bb2f882363cde8c096c8da28f
GiorgiBezhanishvili <giorgi.bezhanishvili6@gmail.com>
2020-05-08 15:43:58 +0400

    Add project files.

---
+++ XO_Game/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace XO_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxAdd = 9;
+
+            XOGame XO = new XOGame();
+
+            bool nextTurn = true;
+
+            do
+            {
+
+                XO.PrintDesign();
+
+                if (XO.CheckTable() == null)
+                {
+
+                    if (nextTurn == true)
+                    {
+
+                    reInsertX:
+                        Console.WriteLine("X turn : ");
+
+                        int indexI = Convert.ToInt32(Console.ReadLine()) - 1;
+                        int indexJ = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        if (XO.Table[indexI, indexJ] == 'x' || XO.Table[indexI, indexJ] == 'o')
+                        {
+                          
+                            Console.WriteLine("Enter Valid Coordinates");
+                            goto reInsertX;
+                        }
+
+                        XO.Table[indexI, indexJ] = 'x';
+                        nextTurn = false;
+                    }
+                    else
+                    {
+                    reInsertO:
+                        Console.WriteLine("O turn : ");
+
+                        int indexI = Convert.ToInt32(Console.ReadLine()) - 1;
+                        int indexJ = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        if (XO.Table[indexI, indexJ] == 'x' || XO.Table[indexI, indexJ] == 'o')
+                        {
+                      
+                            Console.WriteLine("Enter Valid Coordinates");
+                            goto reInsertO;
+                        }
+
+                        XO.Table[indexI, indexJ] = 'o';
+                        nextTurn = true;
+                    }
+                }
+                else
+                {
+                    if (XO.CheckTable() == "Win X")
+                    {
+                        Console.WriteLine("Win X");
+                    }
+                    else
+                    {
+                        Console.WriteLine(XO.CheckTable());
+                    }
+                    break;
+                }
+
+            } while (maxAdd-- > 0);
+
+        }
+    }
+}
---
+++ XO_Game/XOGame.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XO_Game
+{
+    class XOGame
+    {
+        public static int n = 3;
+        private int countX;
+        private int countO;
+        public char[,] Table = new char[n, n];
+
+        // adding X in Table
+        public void addX(int i,int j) {
+            Table[i,j] = 'x';
+        }
+        // adding O in Table
+        public void addO(int i,int j) {
+            Table[i, j] = 'o';
+        }
+
+
+        public string CheckTable() {
+            //horizontal check
+            { 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // x check
+                    if (Table[i,j] == 'x') {
+                        countX++;
+                        if (countX == 3) {
+                            return "Win X";
+                        }
+                    }
+
+                    // o check
+                    if (Table[i,j] == 'o') {
+                        countO++;
+                        if (countO == 3) {
+                            return "Win O";
+                        }
+                    }
+                }
+                
+                countX = 0;
+                countO = 0;
+            }
+            }
+            //vertical check
+            { 
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    // x check
+                    if (Table[i,j] == 'x') {
+                        countX++;
+                        if (countX == 3) {
+                            return "Win X";
+                        }
+                    }
+                    // o check
+                    if (Table[i, j] == 'o')
+                    {
+                        countO++;
+                        if (countO == 3)
+                        {
+                            return "Win O";
+                        }
+                    }
+                }
+
+                countX = 0;
+                countO = 0;
+            }
+            }
+            //diagonal check (\)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        // (\) diagonal
+                        if (i == j)
+                        {
+                            // x check
+                            if (Table[i, j] == 'x')
+                            {
+                                countX++;
+                                if (countX == 3)
+                                {
+                                    return "Win X";
+                                }
+                            }
+
+                            // o check
+                            if (Table[i, j] == 'o')
+                            {
+                                countO++;
+                                if (countO == 3)
+                                {
+                                    return "Win O";
+                                }
+                            }
+                        }
+                    }
+                }
+                countX = 0;
+                countO = 0;
+            }
+
+            //diagonal check (/)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        // (/) diagonal
+                        if ((i + j) == n - 1)
+                        {
+                            // x check
+                            if (Table[i, j] == 'x')
+                            {
+                                countX++;
+                                if (countX == 3)
+                                {
+                                    return "Win X";
+                                }
+                            }
+
+                            // o check
+                            if (Table[i, j] == 'o')
+                            {
+                                countO++;
+                                if (countO == 3)
+                                {
+                                    return "Win O";
+                                }
+                            }
+                        }
+                    }
+                }
+                countX = 0;
+                countO = 0;
+
+            }
+
+            return null;
+        }
+
+        public void PrintDesign() {
+            Console.WriteLine($"  1 2 3 ");
+            Console.WriteLine($"1|{Table[0, 0]}|{Table[0, 1]}|{Table[0, 2]}|");
+            Console.WriteLine($" |-----|");
+            Console.WriteLine($"2|{Table[1, 0]}|{Table[1, 1]}|{Table[1, 2]}|");
+            Console.WriteLine($" |-----|");
+            Console.WriteLine($"3|{Table[2, 0]}|{Table[2, 1]}|{Table[2, 2]}|");
+        }
+
+    }
+}
